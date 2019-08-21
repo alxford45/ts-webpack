1b35511fa42f9e07dd20d36670b1a5e8fb6676b7
Alex <alxford45@gmail.com>
2019-08-21 13:43:12 -0500

    cleaned and reorganized config

--- config/webpack.config.ts
+++ config/webpack.config.ts
@@ -1,22 +1,53 @@
-import { Configuration } from "./config.types";
+import nodeExternals from "webpack-node-externals";
+import { Configuration } from "webpack";
+
 import {
-  ENTRY,
-  MODE,
-  NAME,
+  server,
   TSLoaderRules,
+  web,
   devServer,
-  htmlWebpackPlugin
+  types,
+  nodemonPlugin,
+  htmlWebpackPlugin,
+  config
 } from "./config";
 
-const config: Configuration = {
-  name: NAME,
-  mode: MODE,
-  entry: ENTRY,
-  devServer: devServer,
-  module: {
-    rules: [TSLoaderRules]
+const configuration: Configuration[] = [
+  {
+    name: server.name,
+    entry: server.entry,
+    context: server.context,
+    mode: "development",
+    module: {
+      rules: [TSLoaderRules]
+    },
+    resolve: {
+      extensions: [".ts, .js"],
+      alias: {
+        types: types,
+        config: config
+      }
+    },
+    target: "node",
+    externals: [nodeExternals()],
+    plugins: [nodemonPlugin]
   },
-  plugins: [htmlWebpackPlugin]
-};
+  {
+    name: web.name,
+    entry: web.entry,
+    mode: "development",
+    devServer: devServer,
+    module: {
+      rules: [TSLoaderRules]
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".jsx"],
+      alias: {
+        types: types
+      }
+    },
+    plugins: [htmlWebpackPlugin]
+  }
+];
 
-export default config;
+export default configuration;
